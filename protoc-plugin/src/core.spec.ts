fd1456551265739ea809cf19c0625e41e831753e
Ezard <ezard.ben@gmail.com>
2020-10-23 18:40:18 +0100

    Add tests for functions in core.ts

--- protoc-plugin/src/core.spec.ts
+++ protoc-plugin/src/core.spec.ts
@@ -1,5 +1,9 @@
 import { existsSync, rmdirSync, unlinkSync, writeFileSync } from 'fs';
-import { loadServices, Service } from './core';
+import { google } from 'ts-proto/build/pbjs';
+import { determineServices, loadServices, Service, Services } from './core';
+import FileDescriptorProto = google.protobuf.FileDescriptorProto;
+import SourceCodeInfo = google.protobuf.SourceCodeInfo;
+import Location = google.protobuf.SourceCodeInfo.Location;
 
 describe('core', () => {
   describe('Service', () => {
@@ -69,6 +73,117 @@ describe('core', () => {
       rmdirSync(result['bar'].generatedDir);
       rmdirSync(result['bar'].protosDir);
       rmdirSync(barRootDir);
+    });
+  });
+
+  describe('determineServices', () => {
+    const services: Services = {
+      fooBackend: new Service('./determineServices-test-fooBackend'),
+      fooFrontend: new Service('./determineServices-test-fooFrontend'),
+      barBackend: new Service('./determineServices-test-barBackend'),
+      barFrontend: new Service('./determineServices-test-barFrontend'),
+    };
+    it('should parse a single backend service', () => {
+      const fileDescriptorProto = new FileDescriptorProto({
+        sourceCodeInfo: new SourceCodeInfo({
+          location: [
+            new Location({
+              leadingDetachedComments: ['backend-services=fooBackend'],
+            }),
+          ],
+        }),
+      });
+
+      const result = determineServices(services, fileDescriptorProto);
+      const fooBackendDetermined = result.backendServices.includes(services['fooBackend']);
+
+      expect(fooBackendDetermined).toEqual(true);
+    });
+    it('should parse a multiple backend services', () => {
+      const fileDescriptorProto = new FileDescriptorProto({
+        sourceCodeInfo: new SourceCodeInfo({
+          location: [
+            new Location({
+              leadingDetachedComments: ['backend-services=fooBackend,barBackend'],
+            }),
+          ],
+        }),
+      });
+
+      const result = determineServices(services, fileDescriptorProto);
+      const fooBackendDetermined = result.backendServices.includes(services['fooBackend']);
+      const barBackendDetermined = result.backendServices.includes(services['barBackend']);
+
+      expect(fooBackendDetermined).toEqual(true);
+      expect(barBackendDetermined).toEqual(true);
+    });
+    it('should parse a single frontend service', () => {
+      const fileDescriptorProto = new FileDescriptorProto({
+        sourceCodeInfo: new SourceCodeInfo({
+          location: [
+            new Location({
+              leadingDetachedComments: ['frontend-services=fooFrontend'],
+            }),
+          ],
+        }),
+      });
+
+      const result = determineServices(services, fileDescriptorProto);
+      const fooFrontendDetermined = result.frontendServices.includes(services['fooFrontend']);
+
+      expect(fooFrontendDetermined).toEqual(true);
+    });
+    it('should parse a multiple frontend services', () => {
+      const fileDescriptorProto = new FileDescriptorProto({
+        sourceCodeInfo: new SourceCodeInfo({
+          location: [
+            new Location({
+              leadingDetachedComments: ['frontend-services=fooFrontend,barFrontend'],
+            }),
+          ],
+        }),
+      });
+
+      const result = determineServices(services, fileDescriptorProto);
+      const fooFrontendDetermined = result.frontendServices.includes(services['fooFrontend']);
+      const barFrontendDetermined = result.frontendServices.includes(services['barFrontend']);
+
+      expect(fooFrontendDetermined).toEqual(true);
+      expect(barFrontendDetermined).toEqual(true);
+    });
+    it('should parse multiple backend services and multiple frontend services', () => {
+      const fileDescriptorProto = new FileDescriptorProto({
+        sourceCodeInfo: new SourceCodeInfo({
+          location: [
+            new Location({
+              leadingDetachedComments: [
+                `backend-services=fooBackend,barBackend\nfrontend-services=fooFrontend,barFrontend`,
+              ],
+            }),
+          ],
+        }),
+      });
+
+      const result = determineServices(services, fileDescriptorProto);
+      const fooBackendDetermined = result.backendServices.includes(services['fooBackend']);
+      const barBackendDetermined = result.backendServices.includes(services['barBackend']);
+      const fooFrontendDetermined = result.frontendServices.includes(services['fooFrontend']);
+      const barFrontendDetermined = result.frontendServices.includes(services['barFrontend']);
+
+      expect(fooBackendDetermined).toEqual(true);
+      expect(barBackendDetermined).toEqual(true);
+      expect(fooFrontendDetermined).toEqual(true);
+      expect(barFrontendDetermined).toEqual(true);
+    });
+    it('should return empty arrays when no services are parsed', () => {
+      const fileDescriptorProto = new FileDescriptorProto();
+
+      const result = determineServices(services, fileDescriptorProto);
+      const numBackendServices = result.backendServices.length;
+      const numFrontendServices = result.frontendServices.length;
+
+      expect(numBackendServices).toEqual(0);
+      expect(numFrontendServices).toEqual(0);
     });
   });
 });
