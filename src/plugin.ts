693a0ecfc7849813ee380c7b1c0ae7017d0a7cb5
Ezard <ezard.ben@gmail.com>
2020-10-25 23:12:03 +0000

    Swap load order of services file and protos dir

--- src/plugin.ts
+++ src/plugin.ts
@@ -11,14 +11,14 @@ async function main() {
   const request = CodeGeneratorRequest.decode(input);
   const args = request.parameter.split(',');
 
-  const protosDir = args.find(arg => /protos_dir=.+/.test(arg))?.split('=')?.[1];
-  if (!protosDir) {
-    throw new Error('"protos_dir" parameter must be specified e.g. --ts_proto_opt=protos_dir=../protos');
-  }
-
   const servicesFile = args.find(arg => /services_file=.+/.test(arg))?.split('=')?.[1];
   if (!servicesFile) {
     throw new Error('"services_file" parameter must be specified e.g. --ts_proto_opt=services_file=services.json');
+  }
+
+  const protosDir = args.find(arg => /protos_dir=.+/.test(arg))?.split('=')?.[1];
+  if (!protosDir) {
+    throw new Error('"protos_dir" parameter must be specified e.g. --ts_proto_opt=protos_dir=../protos');
   }
 
   const files = await generateFiles(request.protoFile, servicesFile, protosDir);
